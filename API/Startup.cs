42c89ffe94aa8e7889fa462eacb5a8f5ab8b2635
ttandel@criusenergy.com <ttandel@criusenergy.com>
2020-05-10 02:00:05 -0400

    initial commt

--- API/Startup.cs
+++ API/Startup.cs
@@ -27,11 +27,11 @@ namespace API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllers();
             services.AddDbContext<DataContext>(opt =>
             {
                 opt.UseSqlite(Configuration.GetConnectionString("DefaultConnection"));
             });
+            services.AddControllers();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
